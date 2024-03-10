6c71793d6c5e34ebf214c02570a5a410f5c970ee
VietNguyenSet1D <vnguyen141@my.bcit.ca>
2024-03-10 00:09:21 -0800

    Complete

--- js/client.js
+++ js/client.js
@@ -1,6 +1,13 @@
+window.uMessage = user_facing_message;
+
 class Client {
     constructor() {
         this.xhttp = new XMLHttpRequest();
+        this.insert = "INSERT";
+        this.select = "SELECT";
+        if(localStorage.getItem("capacity") == null) {
+            localStorage.setItem("capacity", "0");
+        }
 
         const preset_button = document.getElementById("pre-set-row");
         const submit_query = document.getElementById("submit-query");
@@ -17,36 +24,92 @@ class Client {
     }
 
     insertPresetQuery() {
-        // this.xhttp.open("GET", ``, true);
-        // this.xhttp.send();
+        let capacity = parseInt(localStorage.getItem("capacity"));
+        let status_area = document.getElementById("status");
+        const sarah = `'${capacity+1}', 'Sarah Brown', '1901-01-01'`;
+        const john = `'${capacity+2}', 'John Smith', '1941-01-01'`;
+        const jack = `'${capacity+3}', 'Jack Ma', '1961-01-30'`;
+        const elon = `'${capacity+4}', 'Elon Musk', '1999-01-01'`;
+        const preset_query = `INSERT INTO patients (patientid, name, dateOfBirth) VALUES (${sarah}), (${john}), (${jack}), (${elon})`;        
+        capacity+=4;
+        localStorage.setItem("capacity", JSON.stringify(capacity));
+        
+        this.xhttp.open("POST", "https://strahd2.com/COMP4537/labs/5/api/database/", true);
+        this.xhttp.send(`?command=${preset_query}`);
 
-        // this.xhttp.onreadystatechange = function () {
-        //    if(this.readyState == 4 && this.status == 200) {
-        //     // TBD
-        //    }
-        // }
+        this.xhttp.onload = function () {
+            const response = JSON.parse(this.responseText)
+            status_area.innerHTML = uMessage.STATUS + response.response;
+        }
     }
 
     submitQuery(query) {
-        const firstSpaceIndex = query.indexOf(' ');
-        const firstWord = query.substring(0, firstSpaceIndex);
-        
-        if(firstWord == "INSERT") {
-            // this.xhttp.open("POST", "");
-            // this.xhttp.send(query);
-        
-            // this.xhttp.onload = function () {
-            //     //TBD
-            // }
-        } else {
-        // this.xhttp.open("GET", ``, true);
-        // this.xhttp.send();
+        let status_area = document.getElementById('status');
+        console.log(encodeURI(query.toLowerCase()));
 
-        // this.xhttp.onreadystatechange = function () {
-        //    if(this.readyState == 4 && this.status == 200) {
-        //     // TBD
-        //    }
-        // }
+        const firstWord = this.extractQuery(query);
+        console.log(firstWord);
+        if(firstWord == "INSERT") {
+            this.xhttp.open("POST", "https://strahd2.com/COMP4537/labs/5/api/database/", true);
+            this.xhttp.send(`?command=${query}`);
+            
+            this.xhttp.onload = function () {
+                const response = JSON.parse(this.responseText)
+                status_area.innerHTML = uMessage.STATUS + response.response;
+            }
+        } else if(firstWord == "SELECT") {
+            this.xhttp.open("GET", `https://strahd2.com/COMP4537/labs/5/api/database?command=${encodeURI(query.toLowerCase())}`, true);
+            this.xhttp.send();
+
+            this.xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    const response = JSON.parse(this.responseText); // Parse JSON response
+                    console.log(response);
+                    let tableresult = "";
+                    for (let i = 0; i < response.response.length; i++) {
+                        tableresult +=
+                            `<tr>
+                                <td>${response.response[i].patientid}</td>
+                                <td>${response.response[i].name}</td>
+                                <td>${response.response[i].dateOfBirth}</td>
+                             </tr>`;
+                    
+                    }                    
+                    document.getElementById('result-table').innerHTML = tableresult;
+                }
+            };
+            
+        } else {
+            window.alert(uMessage.INVALID_QUERY);
+        }
+    }
+
+    // Check if the first word of the query is SELECT or INSERT
+    extractQuery(query) {
+        let firstWord = ""
+        for(let i = 0 ; i < this.insert.length; i++) {
+            if(this.insert[i] == query[i]){
+                firstWord += this.insert[i];
+            } else {
+                firstWord = "";
+                break;
+            }
+        }
+        if(firstWord.length == 0) {
+            for(let i = 0 ; i < this.select.length; i++) {
+                if(this.select[i] == query[i]){
+                    firstWord += this.select[i];
+                } else {
+                    firstWord = "";
+                    break;
+                }
+            }
+        }
+
+        if(firstWord.length != 0) {
+            return firstWord;
+        }else {
+            return "invalid";
         }
     }
 }
--- lang/message/en/user.js
+++ lang/message/en/user.js
@@ -1,3 +1,4 @@
 const user_facing_message = {
-    
+    INVALID_QUERY: "INVALID QUERY: only use SELECT and INSERT", 
+    STATUS: "STATUS: "
 }
